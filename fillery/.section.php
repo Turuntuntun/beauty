103a80ef96c12001ed640b0e6abf714f9b8df05c
root <root@rastudio-dev.ru>
2021-01-18 12:19:27 +0300

    add pages

---
+++ fillery/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "Филлеры";
+$arDirProperties = Array(
+
+);
+?>
---
+++ fillery/index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Филлеры");
+?>
+
+Text here....
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
