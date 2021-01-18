19fb6fc1fc6a4916a57fce7c90c3dd4c109b21f8
root <root@rastudio-dev.ru>
2021-01-18 17:28:36 +0300

    Change small mistake

--- local/templates/beauty/components/bitrix/news.detail/main_about/template.php
+++ local/templates/beauty/components/bitrix/news.detail/main_about/template.php
@@ -34,7 +34,7 @@ $this->setFrameMode(true);
             <?endif;?>
                 <div class="about-info__text"><?=$arResult["DETAIL_TEXT"];?>
                     <a class="about-info__btn" href="/about">
-                        <?=GetMessage('NAME_BUTTON')?>><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <?=GetMessage('NAME_BUTTON')?><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M14.5303 6.53375C14.8232 6.24085 14.8232 5.76598 14.5303 5.47309L9.75736 0.700117C9.46447 0.407224 8.98959 0.407224 8.6967 0.700117C8.40381 0.993011 8.40381 1.46788 8.6967 1.76078L12.9393 6.00342L8.6967 10.2461C8.40381 10.539 8.40381 11.0138 8.6967 11.3067C8.98959 11.5996 9.46447 11.5996 9.75736 11.3067L14.5303 6.53375ZM0 6.75342H14V5.25342H0V6.75342Z" fill="#496DC8"/>
                         </svg>
                     </a>
