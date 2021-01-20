77231964bf21dae6ba2b842a43a566b37c321398
root <root@rastudio-dev.ru>
2021-01-20 12:44:30 +0300

    change params include component

--- auth/index.php
+++ auth/index.php
@@ -2,12 +2,13 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Авторизация");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.auth.form",
-	".default",
-	Array(
-		"AUTH_FORGOT_PASSWORD_URL" => "",
+	"bitrix:main.auth.form", 
+	"auth", 
+	array(
+		"AUTH_FORGOT_PASSWORD_URL" => "/forgot/",
 		"AUTH_REGISTER_URL" => "/registration/",
 		"AUTH_SUCCESS_URL" => "/auth/",
-		"COMPONENT_TEMPLATE" => ".default"
-	)
+		"COMPONENT_TEMPLATE" => "auth"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
