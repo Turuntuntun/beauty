0c1e04737108f4ec7bb1c619ffd802e8c9e6be76
root <root@rastudio-dev.ru>
2021-01-21 18:12:47 +0300

    change temlate top menu

--- local/templates/beauty/components/bitrix/menu/top_menu/template.php
+++ local/templates/beauty/components/bitrix/menu/top_menu/template.php
@@ -1,8 +1,29 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?php
+if (!empty($arResult['ITEMS'])):?>
+    <nav class="header-menu">
+        <?foreach ($arResult['ITEMS'] as $key => $item) :?>
+            <div class="header-menu-item">
+                <a class="header-menu-item__link <?if($item['SELECTED']) echo 'header-menu-item_active';?>" href="<?=$item['LINK']?>">
+                    <?=$item['TEXT']?>
+                </a>
+
+            <?if ($item['CHILDS']):?>
+                <div class="header-menu-item-list">
+                    <div class="header-menu-item-list-links">
+                    <?foreach ($item['CHILDS']as $id => $child) :?>
+                        <a class="header-menu-item-list-links__link" href="<?=$child['LINK']?>">
+                            <?=$child['TEXT']?>
+                        </a>
+                    <?endforeach;?>
+                    </div>
+                </div>
+            <? endif;?>
+            </div>
+        <?endforeach;?>
+    </nav>
 
 
-if (!empty($arResult)):?>
     <div class="header-menu--mobile">
         <?foreach($arResult as $arItem):
         if($arParams["MAX_LEVEL"] == 1 && $arItem["PARAMS"])
