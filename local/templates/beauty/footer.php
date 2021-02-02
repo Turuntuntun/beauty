1d7157fa33c6c49cb73ac0c708e9f72a3335ef2d
root <root@rastudio-dev.ru>
2021-02-02 17:55:05 +0300

    add componetn subscribe

--- local/templates/beauty/footer.php
+++ local/templates/beauty/footer.php
@@ -17,14 +17,30 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
                     <div class="footer-links"><a class="footer__link" href="#">Средства индивидуальной защиты</a><a class="footer__link" href="#">Шапочки одноразовые</a><a class="footer__link" href="#">Халаты одноразовые</a><a class="footer__link" href="#">Перчатки одноразовые</a><a class="footer__link" href="#">Маски одноразовые</a><a class="footer__link" href="#">Антисептики и дезинфекторы</a><a class="footer__link" href="#">Бахилы</a></div>
                 </div>
                 <div class="footer-col"><a class="footer__title" href="#">О нас</a><a class="footer__title" href="#">Контакты</a><a class="footer__title" href="#">Цены</a><a class="footer__title" href="#">Акции</a></div>
-                <div class="footer-col">
-                    <div class="footer__title">Будьте всегда первыми в курсе всех наших акций и новостей</div>
-                    <form class="footer-subscribe" action="#">
-                        <input type="email" data-type="email" data-required placeholder="Введите ваш e-mail" required>
-                        <button class="btn-background" type="submit">Подписаться</button>
-                    </form>
-                    <div class="footer__email">Наша почта: <a href="mailto:admin@beatu-guard.ru">admin@beatu-guard.ru</a></div>
-                </div>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:sender.subscribe",
+                    "main_subscribe",
+                    array(
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_ADDITIONAL" => "",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "CACHE_TIME" => "3600",
+                        "CACHE_TYPE" => "A",
+                        "CONFIRMATION" => "Y",
+                        "HIDE_MAILINGS" => "N",
+                        "SET_TITLE" => "N",
+                        "SHOW_HIDDEN" => "N",
+                        "USER_CONSENT" => "N",
+                        "USER_CONSENT_ID" => "0",
+                        "USER_CONSENT_IS_CHECKED" => "Y",
+                        "USER_CONSENT_IS_LOADED" => "N",
+                        "USE_PERSONALIZATION" => "Y",
+                        "COMPONENT_TEMPLATE" => "main_subscribe"
+                    ),
+                    false
+                );?>
             </div>
             <div class="footer-row">
                 <div class="footer__text">Beauty Guard. © 2020</div>
